fbfcec9fa7b65bef120342bc41b09cd49c78b836
aleksi-lukkarinen <aleksi.lukkarinen@gmail.com>
2022-04-25 00:31:16 +0300

    Adds a body to parsePlainDate()

--- src/prices.mjs
+++ src/prices.mjs
@@ -30,7 +30,8 @@ function createApp(database) {
   }
 
   function parsePlainDate(dateString) {
-    if (dateString) { }
+    if (dateString) {
+    }
   }
 
   function calculateCost(age, type, date, baseCost) {
