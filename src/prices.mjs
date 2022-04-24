28b7dc78ef8f6f1f541e239143a27b212218f245
aleksi-lukkarinen <aleksi.lukkarinen@gmail.com>
2022-04-24 21:27:26 +0300

    Removes the body of parsePlainDate()

--- src/prices.mjs
+++ src/prices.mjs
@@ -30,7 +30,7 @@ function createApp(database) {
   }
 
   function parsePlainDate(dateString) {
-    if (dateString) { return Temporal.PlainDate.from(dateString); }
+    if (dateString) { }
   }
 
   function calculateCost(age, type, date, baseCost) {
