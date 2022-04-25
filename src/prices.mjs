e038b4954f5fe21f4268c0d730b5b311139a3167
aleksi-lukkarinen <aleksi.lukkarinen@gmail.com>
2022-04-25 13:14:51 +0300

    Return to original state

--- src/prices.mjs
+++ src/prices.mjs
@@ -18,7 +18,7 @@ function createApp(database) {
     const age = req.query.age;
     const type = req.query.type;
     const baseCost = database.findBasePriceByType(type).cost;
-    const date = parsePlainDate(req.query.date);
+    const date = parseDate(req.query.date);
     const cost = calculateCost(age, type, date, baseCost);
     res.json({ cost });
   });
@@ -26,12 +26,6 @@ function createApp(database) {
   function parseDate(dateString) {
     if (dateString) {
       return new Date(dateString);
-    }
-  }
-
-  function parsePlainDate(dateString) {
-    if (dateString) {
-      return Temporal.PlainDate.from(dateString);
     }
   }
 
@@ -82,18 +76,18 @@ function createApp(database) {
   }
 
   function isMonday(date) {
-    return date.dayOfWeek === 1;
+    return date.getDay() === 1;
   }
 
   function isHoliday(date) {
     const holidays = database.getHolidays();
     for (let row of holidays) {
-      const holiday = parsePlainDate(row.holiday);
+      let holiday = new Date(row.holiday);
       if (
         date &&
-        date.year === holiday.year &&
-        date.month === holiday.month &&
-        date.day === holiday.day
+        date.getFullYear() === holiday.getFullYear() &&
+        date.getMonth() === holiday.getMonth() &&
+        date.getDate() === holiday.getDate()
       ) {
         return true;
       }
