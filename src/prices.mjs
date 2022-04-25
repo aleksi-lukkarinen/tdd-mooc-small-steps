7d42cbe3523b49ac6e2ba97928cae05ee52462e6
aleksi-lukkarinen <aleksi.lukkarinen@gmail.com>
2022-04-26 01:51:57 +0300

    Converts the arg of calculateCostForDayTicket()

--- src/prices.mjs
+++ src/prices.mjs
@@ -33,7 +33,7 @@ function createApp(database) {
     if (type === "night") {
       return calculateCostForNightTicket(age, baseCost);
     } else {
-      return calculateCostForDayTicket(age, date, baseCost);
+      return calculateCostForDayTicket(age, c(date), baseCost);
     }
   }
 
@@ -51,7 +51,7 @@ function createApp(database) {
   }
 
   function calculateCostForDayTicket(age, date, baseCost) {
-    let reduction = calculateReduction(c(date));
+    let reduction = calculateReduction(date);
     if (age === undefined) {
       return Math.ceil(baseCost * (1 - reduction / 100));
     }
